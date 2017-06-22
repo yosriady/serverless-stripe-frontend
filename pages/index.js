eb5657472370030003fcf2857659a11723584d3e
Yos Riady <yos.riady@bandlab.com>
2017-06-22 11:40:55 +0800

    Revert react-stripe-elements due to SSR undefined window issue

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from 'next/head';
-import { StripeProvider } from 'react-stripe-elements';
-
-import MyCheckout from '../components/MyCheckout';
 
 export default () => (
   <div>
@@ -10,8 +7,5 @@ export default () => (
       <script src="https://js.stripe.com/v3/" />
     </Head>
     <p> Welcome to next.js!</p>
-    <StripeProvider apiKey="pk_test_12345">
-      <MyCheckout />
-    </StripeProvider>
   </div>
 );
